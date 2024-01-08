a1c81c309e298fd878b1fdab4502c4c122f13467
jaquesWD <1622376149@qq.com>
2024-01-09 01:02:41 +0800

    封装treeSelect

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+import wdTree from './components/treeSelect.vue'
+import WdDemo from './components/demo.vue'
+
+const list = ref([
+  {
+    name: '系统',
+    id: 1,
+    children: [
+      { name: '用户', id: 2 },
+      { name: '用户组', id: 3 },
+      { name: '角色', id: 4 },
+      { name: '菜单', id: 5 },
+      { name: '组织架构', id: 6 }
+    ]
+  },
+  {
+    name: '购物',
+    id: 11,
+    children: [
+      { name: '菜', id: 12 },
+      { name: '苹果', id: 13 },
+      { name: '西瓜', id: 14 },
+      { name: '火龙果', id: 15 },
+      { name: '雪梨', id: 16 }
+    ]
+  }
+]
+)
+
+const changeNode = (value) => {
+  console.log(value);
+}
+</script>
+
+<template>
+  <wdTree :options="list" :multiple="true" @handleNodeClick="changeNode"/>
+  <!-- <WdDemo :options="list" :multiple="true"/> -->
+</template>
+
+<style scoped></style>
